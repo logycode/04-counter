42aa1748e29b96feaf587b889abe30135f5d615a
Tim Kranich <tim@logyco.de>
2023-01-18 23:46:15 +0100

    adds different color for counter > 100

--- 04-counter.js
+++ 04-counter.js
@@ -4,7 +4,13 @@ let num = 0;
 function count() {
   num++;
   counter.textContent = num;
-  document.querySelector(".counter-display").style.width = num + "vw";
+  if (num > 100) {
+    document.querySelector(".counter-display").style.width = num - 100 + "vw";
+    document.querySelector(".counter-display").classList.add("bg100");
+    document.querySelector("header").classList.add("bg100");
+  } else {
+    document.querySelector(".counter-display").style.width = num + "vw";
+  }
 }
 
 document.querySelector("main").addEventListener("click", count); // warum muss ich hier auf die Klammern verzichten, um die Funktion immer wieder auszuführen?
@@ -17,4 +23,7 @@ document.addEventListener("keyup", (event) => {
 document.querySelector(".reset").addEventListener("click", () => {
   num = 0;
   counter.textContent = num;
+  document.querySelector(".counter-display").style.width = 0;
+  document.querySelector(".counter-display").classList.remove("bg100");
+  document.querySelector("header").classList.remove("bg100");
 });
